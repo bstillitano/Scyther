173064491757a06504cf0e4e4e4d8f636494a2e1
Brandon Stillitano <b.stillitano95@gmail.com>
2020-12-22 21:56:48 +1100

    Lower deployment target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Scyther",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
     ], products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
