76775a6e8faff2145694ccc6f7877d985f812c74
Brandon Stillitano <b.stillitano95@gmail.com>
2020-12-28 01:47:42 +1100

    More safety

--- Sources/Scyther/Models/UITableViewCellStyle.swift
+++ Sources/Scyther/Models/UITableViewCellStyle.swift
@@ -5,7 +5,9 @@
 //  Created by Brandon Stillitano on 25/12/20.
 //
 
+#if !os(macOS)
 import UIKit
 
 /// Convenience `typealias` for accessing `UITableViewCell.CellStyle`
 typealias UITableViewCellStyle = UITableViewCell.CellStyle
+#endif
