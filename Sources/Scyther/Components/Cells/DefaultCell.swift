8b6f7a33b0f3c50b1c69353441af3b120b6d735a
Brandon Stillitano <b.stillitano95@gmail.com>
2020-12-27 23:34:13 +1100

    Remove custom constraints

--- Sources/Scyther/Components/Cells/DefaultCell.swift
+++ Sources/Scyther/Components/Cells/DefaultCell.swift
@@ -16,6 +16,7 @@ final internal class DefaultCell: UITableViewCell {
         textLabel?.numberOfLines = 0
         textLabel?.adjustsFontSizeToFitWidth = false
         detailTextLabel?.adjustsFontSizeToFitWidth = true
+        detailTextLabel?.numberOfLines = 0
         detailTextLabel?.minimumScaleFactor = 2
     }
 
@@ -29,38 +30,6 @@ final internal class DefaultCell: UITableViewCell {
         textLabel?.text = nil
         detailTextLabel?.text = nil
         imageView?.image = nil
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        /// Update text label constraints
-        guard textLabel?.superview != nil else {
-            return
-        }
-        textLabel?.snp.remakeConstraints({ (make) in
-            make.top.equalToSuperview().inset(8)
-            make.bottom.equalToSuperview().inset(8)
-            make.width.lessThanOrEqualTo(160)
-            
-            /// Set conditional constraints
-            if imageView?.image != nil {
-                make.left.equalTo(imageView?.snp.right ?? 0).offset(16)
-            } else {
-                make.left.equalToSuperview().inset(16)
-            }
-        })
-        
-        /// Update detail text label constraints
-        guard detailTextLabel?.superview != nil else {
-            return
-        }
-        detailTextLabel?.snp.remakeConstraints({ (make) in
-            make.top.equalToSuperview().inset(8)
-            make.bottom.equalToSuperview().inset(8)
-            make.right.equalToSuperview().inset(16)
-            make.left.equalTo(textLabel?.snp.right ?? 0).offset(16)
-        })
     }
 }
 #endif
