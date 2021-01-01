59ad85b841912f9d3e966893e122971b0eafef75
Brandon Stillitano <b.stillitano95@gmail.com>
2021-01-01 20:37:19 +1100

    Revert "Revert swizzling safety"
    This reverts commit 3a600c35a069ba33172f122a8bf14ca13775da5e.

--- Sources/Scyther/Utilities/Network Logger/LoggerHTTPModelManager.swift
+++ Sources/Scyther/Utilities/Network Logger/LoggerHTTPModelManager.swift
@@ -7,30 +7,27 @@
 
 import Foundation
 
-private let _sharedInstance = LoggerHTTPModelManager()
-
 final class LoggerHTTPModelManager: NSObject {
     static let sharedInstance = LoggerHTTPModelManager()
-    fileprivate var models = [LoggerHTTPModel]()
+    private var requestModels = [LoggerHTTPModel]()
     private let syncQueue = DispatchQueue(label: "LoggerSyncQueue")
     
     func add(_ obj: LoggerHTTPModel) {
         syncQueue.async {
-            self.models.insert(obj, at: 0)
+            self.requestModels.insert(obj, at: 0)
             NotificationCenter.default.post(name: NSNotification.Name.LoggerAddedModel, object: obj)
         }
     }
     
     func clear() {
         syncQueue.async {
-            self.models.removeAll()
+            self.requestModels.removeAll()
             NotificationCenter.default.post(name: NSNotification.Name.LoggerClearedModels, object: nil)
         }
     }
     
-    func getModels() -> [LoggerHTTPModel] {
+    var models: [LoggerHTTPModel] {
         var predicates = [NSPredicate]()
-        
         let filterValues = Logger.instance.cachedFilters
         let filterNames = HTTPModelShortType.allCases
         
@@ -46,8 +43,7 @@ final class LoggerHTTPModelManager: NSObject {
         }
 
         let searchPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: predicates)
-        
-        let array = (self.models as NSArray).filtered(using: searchPredicate)
+        let array = (self.requestModels as NSArray).filtered(using: searchPredicate)
         
         return array as? [LoggerHTTPModel] ?? []
     }
