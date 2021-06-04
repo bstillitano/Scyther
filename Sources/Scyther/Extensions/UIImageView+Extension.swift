bdf3e40d0a143e1e52f10d1436d94db9df51a402
Stefan Haider <stefan.haider@post.at>
2021-06-04 08:23:59 +0200

    removed default image

--- Sources/Scyther/Extensions/UIImageView+Extension.swift
+++ Sources/Scyther/Extensions/UIImageView+Extension.swift
@@ -11,23 +11,19 @@ import UIKit
 extension UIImageView {
     
     /**
-     Trys to load an Image from an URL and sets it as image, if this fails there is a defaultImage you can set.
+     Trys to load an Image from an URL and sets it as image
      
      - Parameters:
         - url: The URL of the image to fetch
-        - defaultImage: The image wich gets set if loading the image from the URL fails.
         - completion: called with true if ether the loaded or the defaultImage is set, false if no image is set.
      */
-    func loadImage(fromURL url: URL, defaultImage: UIImage? = nil, completion: ((Bool) -> Void)? = nil) {
+    func loadImage(fromURL url: URL, completion: ((Bool) -> Void)? = nil) {
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url), let image =  UIImage(data: data) {
                 DispatchQueue.main.async {
                     self?.image = image
                     completion?(true)
                 }
-            } else if let defaultImage = defaultImage {
-                self?.image = defaultImage
-                completion?(true)
             } else {
                 completion?(false)
             }
